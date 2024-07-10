fbfc4756cbf96791026c6c5d681916297e761cc3
Priti Parkhe <parkhepriti47@gmail.com>
2024-07-11 00:30:08 +0530

    resolved issue related address is reuired for stipe payment

--- backend/src/controllers/payment/paymentController.js
+++ backend/src/controllers/payment/paymentController.js
@@ -3,48 +3,60 @@ import userModel from "../../models/userModel.js";
 
 const paymentController = async (req, res) => {
   try {
-    const { cardItems } = req.body;
-    console.log(cardItems, "cardItems");
+    const { cartItems } = req.body;
     const user = await userModel.findOne({ _id: req.userId });
+
     const params = {
       submit_type: "pay",
       mode: "payment",
-      payment_method_types: ["card", "upi"],
+      payment_method_types: ["card"],
       billing_address_collection: "auto",
+      shipping_address_collection: {
+        allowed_countries: ["IN"],
+      },
       shipping_options: [
         {
           shipping_rate: "shr_1PaChiSBSBI4t3q40lVm8pSe",
         },
       ],
       customer_email: user.email,
-      line_items: cardItems.map((item, index) => {
+      metadata: {
+        userId: req.userId,
+      },
+      line_items: cartItems.map((item) => {
         return {
           price_data: {
             currency: "inr",
-            product_data: {},
-            name : item.productId.title,
-            Images : item.productId.productImage,
-            metadata : {
-              productId : item.productId._id
+            product_data: {
+              name: item.productId.title,
+              images: item.productId.productImage,
+              metadata: {
+                productId: item.productId._id,
+              },
             },
-            unit_amount : item.productId.sellingPrice
+            unit_amount: item.productId.sellingPrice * 100,
           },
-          adjustable_quantity : {
-            enabled : true,
-            minimum : 1
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
           },
-          quantity : item.quantity
+          quantity: item.quantity,
         };
       }),
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
     };
+
     const session = await stripe.checkout.sessions.create(params);
+
     res.status(303).json(session);
   } catch (error) {
-    res.status(500).json({
-      message: error.message || error,
+    res.json({
+      message: error?.message || error,
       error: true,
       success: false,
     });
   }
 };
+
 export { paymentController };
