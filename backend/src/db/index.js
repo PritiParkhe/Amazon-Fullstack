801ef1ecd0d2fbcc915d6d46d752e90669a82066
Priti Parkhe <parkhepriti47@gmail.com>
2024-08-15 13:01:13 +0530

    constant file path changed

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
-const connectDB = async() =>{
+import { DB_NAME } from "../../constants";
+const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL} /${DB_NAME}`)
-    console.log(`\n MongoDB connected!! DB HOSt:${connectionInstance.connection.host}`);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL} /${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected!! DB HOSt:${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("MONGODB Connection error", error);
     process.exit(1);
   }
-}
-export default connectDB
+};
+export default connectDB;
